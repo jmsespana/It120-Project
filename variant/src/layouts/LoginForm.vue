2ea31f8ae30d58cccd84818c0d08a01abe3aa78e
Christine Bersano <christineybersano@gmail.com>
2024-12-22 22:49:04 +0800

    style: added ui design

--- variant/src/layouts/LoginForm.vue
+++ variant/src/layouts/LoginForm.vue
@@ -1,32 +1,3 @@
-<template>
-  <v-card class="bg-card" elevation="8">
-    <h1 class="text-center py-8">$Receiver</h1>
-
-    <v-form ref="form" @submit.prevent="handleLogin">
-      <v-text-field
-        v-model="email"
-        color="blue-lighten-2"
-        label="Email"
-        type="email"
-        required
-        outlined
-      />
-      <v-text-field
-        v-model="password"
-        color="blue-lighten-2"
-        label="Password"
-        type="password"
-        required
-        outlined
-      />
-      <v-btn type="submit" color="primary" class="mt-1" size="large" block
-        >Login</v-btn
-      >
-    </v-form>
-    <slot name="footer" />
-  </v-card>
-</template>
-
 <script>
 import { useAuthStore } from "@/stores/auth"; // Importing the merged auth store
 import { ref } from "vue";
@@ -59,6 +30,83 @@ export default {
 };
 </script>
 
-<style scoped>
 
-</style>
+
+<template>
+  <v-card class="pa-4 login-card" style="background-color: #93b1b5;">
+    <img src="/src/images/logo.png" alt="SnapTalk Logo" class="logo" />
+    <slot name="header">
+    </slot>
+    <v-form ref="form" @submit.prevent="handleLogin">
+      <v-text-field
+        v-model="email"
+        label="Email"
+        type="email"
+        required
+        outlined
+        prepend-icon="mdi-email"
+      ></v-text-field>
+      <v-text-field
+        v-model="password"
+        label="Password"
+        type="password"
+        required
+        outlined
+        prepend-icon="mdi-lock"
+      ></v-text-field>
+      <v-btn type="submit" class="mt-4" block prepend-icon="mdi-login">Login</v-btn>
+    </v-form>
+    <br />
+    <slot name="footer"></slot>
+  </v-card>
+  </template>
+  
+  
+  
+  <style scoped>
+  .v-btn {
+    background-color: #0b2e33;
+    color: #fff;
+    font-weight: bold;
+    text-transform: uppercase;
+    border-radius: 8px;
+    padding: 12px 20px;
+  }
+  
+  .logo {
+    display: block;
+    width: 170px;
+    margin: 0 auto -10px auto;
+  }
+  
+  .login-card {
+    padding: 60px;
+    width: 150%;
+    margin-left: -120px;
+    border: 2px solid #93b1b5;
+    border-radius: 10px;
+  }
+  
+  @media (max-width: 768px) {
+    .login-card {
+      padding: 40px;
+      width: 90%;
+    }
+  
+    .login-card h2 {
+      font-size: 2rem;
+    }
+  }
+  
+  @media (max-width: 480px) {
+    .login-card {
+      padding: 30px;
+      width: 90%;
+    }
+  
+    .login-card h2 {
+      font-size: 1.8rem;
+    }
+  }
+  </style>
+  
